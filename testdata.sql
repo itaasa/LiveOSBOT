22f0315a1d63f545ca48373814a8a5917458b049
itaasa <34899051+itaasa@users.noreply.github.com>
2018-04-01 00:02:14 -0400

    Update testdata.sql

--- testdata.sql
+++ testdata.sql
@@ -1,11 +1,17 @@
 INSERT INTO SKILL (SkillID, SkillName)
   VALUES (10, 'Fishing');
+  
+INSERT INTO SKILL (SkillID, SkillName)
+  VALUES (8, 'Woodcutting');
 
 INSERT INTO ITEM (ItemID, ItemName, ItemXP, ItemPrice, SkillID)
   VALUES (377, 'Raw Lobster', 90, 131, 10);
+
+INSERT INTO ITEM (ItemID, ItemName, ItemXP, ItemPrice, SkillID)
+  VALUES (1511, 'Log', 25, 30, 8);
 
 INSERT INTO BOT (BotID, BotName, IsActive, IsOnline, SkillID)
   VALUES (12, 'RedRooster' , 1, 1, 10);
 
 INSERT INTO REPORT (BotID, ItemID, NumOfItems, XpPerHour, GpPerHour)
-  VALUES (12, 377, 3877, 52343.20, 231443.00);
+  VALUES (12, 1511, 0, 0, 0);
