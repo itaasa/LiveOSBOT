223c72c635f83452dd04d78ef4aea686779190b0
itaasa <34899051+itaasa@users.noreply.github.com>
2018-05-17 06:35:27 -0400

    Implemented Login Feature to PHP Script

--- PHP/login.php
+++ PHP/login.php
@@ -1,7 +1,17 @@
 <?php
   include_once("connection.php");
 
-  if ($conn){
-    echo "success";
-  }
+  if( isset($_POST['txtUsername']) && isset($_POST['txtPassword']) ) {
+       $username = $_POST['txtUsername'];
+       $password = $_POST['txtPassword'];
+
+       $query = "SELECT User, Pass FROM user WHERE User = '$username' AND Pass = '$password'";
+
+        $result = mysqli_query($conn, $query);
+
+        if ($result->num_rows > 0)
+          echo "success";
+        else
+          echo "failed";
+}
  ?>
