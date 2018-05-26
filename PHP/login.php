589f83f4c04022515afdaf542c48155696576271
itaasa <34899051+itaasa@users.noreply.github.com>
2018-05-26 14:19:22 -0400

    Protect from SQL Injection

--- PHP/login.php
+++ PHP/login.php
@@ -5,13 +5,18 @@
        $username = $_POST['txtUsername'];
        $password = $_POST['txtPassword'];
 
-       $query = "SELECT User, Pass FROM user WHERE User = '$username' AND Pass = '$password'";
+       $query = "SELECT User, Pass FROM user WHERE User=? AND Pass=?";
 
-        $result = mysqli_prepare($conn, $query);
-
-        if ($result->num_rows > 0)
+        $stmt = mysqli_prepare($conn, "SELECT User, Pass FROM user WHERE User=? AND Pass=?");
+        mysqli_stmt_bind_param($stmt, 'ss', $username, $password);
+        mysqli_stmt_execute($stmt);
+         mysqli_stmt_store_result($stmt);
+        if (mysqli_stmt_num_rows($stmt) > 0)
           echo "success";
         else
           echo "failed";
+
+
+	mysqli_stmt_close($stmt);
 }
  ?>
